b39ae79db2998cfc9b101912e6a201f6d1eb2ae1
Peter Schut <schupeter@users.noreply.github.com>
2017-03-29 17:38:01 -0400

    additional error checks for daily climate files

--- app/models/climate_indices/climate_load.rb
+++ app/models/climate_indices/climate_load.rb
@@ -8,63 +8,66 @@ class Climate_load
 		# read raw data
 		@input = File.read(filename).gsub("\r\n","\n")
 		@yaml = YAML.safe_load(@input)
+		errors = Array.new
+		errors.push({"YAML: Geography" => @yaml["Geography"]}) if @yaml["Geography"] != "stations"
 		
 		@observationsArray = CSV.parse(@input.split("--- #TSV\n")[1], headers:true, header_converters: :symbol, converters: :all, col_sep: "\t")
+		errors.push({"TSV: headers"=>@observationsArray[0].to_hash.keys.collect{|k| k.to_s} } ) if @observationsArray[0].to_hash.keys != [:long, :lat, :elev, :station, :year, :month, :day, :tmax, :tmin, :total_ppt]
 		
-		#@observationsArray = CSV.read(filename, headers:true, header_converters: :symbol, converters: :all, col_sep: "\t")
-		# build a hash to hold data for all stations
-		@observationsHash = Hash.new
-		coordinatesHash = Hash.new
-		require 'net/http'
-		for row in @observationsArray do
-			if row[:station].class == Float then row[:station] = row[:station].to_i end
-			# set up a hash for a new station
-			if @observationsHash.keys.include?(row[:station]) == false then 
-				@observationsHash[row[:station]] = Hash.new
-				coordinatesHash[row[:station]] = {:lat=>row[:lat] ,:long=>row[:long] , :elev=>row[:elev]}
-				coordinatesHash[row[:station]][:ErosivityRegion] = Climate_erosivity.identify_erosivityregion(Web.get_ecoprovince(row[:lat],row[:long]))				
-			end
-			# set up julian day array (with a leading nil)
-			if @observationsHash[row[:station]].keys.include?(row[:year]) == false then @observationsHash[row[:station]][row[:year]] = Array.new(367, nil) end
-			# populate array with data
-			@observationsHash[row[:station]][row[:year]][month2julian[row[:month]]+row[:day]] = {:tmin=>row[:tmin],:tmax=>row[:tmax],:precip=>row[:total_ppt]}
-		end
-		# @observationsHash is populated with all raw data at this point.
-		# for each year, [0] will always be nil, and [60] will be nil during leap years
-		# Now need to:
-		# - collapse Dec 30 & 31 for leap years.
-		# - remove Feb 29 for non-leap years
-		require 'fileutils'
-		errors = Array.new
-		for station in @observationsHash.keys do
-			for year in @observationsHash[station].keys do
-				if Date.leap?(year) then # collapse Dec 30 & 31
-					@observationsHash[station][year][365][:tmin] = (@observationsHash[station][year][365][:tmin] + @observationsHash[station][year][366][:tmin]) / 2
-					@observationsHash[station][year][365][:tmax] = (@observationsHash[station][year][365][:tmax] + @observationsHash[station][year][366][:tmax]) / 2
-					@observationsHash[station][year][365][:precip] = (@observationsHash[station][year][365][:precip] + @observationsHash[station][year][366][:precip])
-					@observationsHash[station][year].delete_at(366)
-				else
-					@observationsHash[station][year].delete_at(60) # remove feb 29
+		if errors.size == 0 then
+			# build a hash to hold data for all stations
+			@observationsHash = Hash.new
+			coordinatesHash = Hash.new
+			require 'net/http'
+			for row in @observationsArray do
+				if row[:station].class == Float then row[:station] = row[:station].to_i end
+				# set up a hash for a new station
+				if @observationsHash.keys.include?(row[:station]) == false then 
+					@observationsHash[row[:station]] = Hash.new
+					coordinatesHash[row[:station]] = {:lat=>row[:lat] ,:long=>row[:long] , :elev=>row[:elev]}
+					coordinatesHash[row[:station]][:ErosivityRegion] = Climate_erosivity.identify_erosivityregion(Web.get_ecoprovince(row[:lat],row[:long]))				
 				end
-				@observationsHash[station][year].delete_at(0) # remove placeholder
-				# save file as CSV
-				dirname = "/production/data/climate/stations/#{station}/daily"
-				FileUtils.mkdir_p(dirname) # create all missing subdirectories
-				# Create a new CSV file populated with observations data
-				CSV.open("#{dirname}/#{year}.csv", 'w', col_sep: "\t") do |csv|
-					csv << ['tmin', 'tmax', 'precip'] # Add headers
-					@observationsHash[station][year].each do |row|
-						if row == nil then
-							errors.push({:station=>[station], :year=>[year]})
-						else
-							csv << [row[:tmin], row[:tmax], row[:precip]]
-						end
+				# set up julian day array (with a leading nil)
+				if @observationsHash[row[:station]].keys.include?(row[:year]) == false then @observationsHash[row[:station]][row[:year]] = Array.new(367, nil) end
+				# populate array with data
+				@observationsHash[row[:station]][row[:year]][month2julian[row[:month]]+row[:day]] = {:tmin=>row[:tmin],:tmax=>row[:tmax],:precip=>row[:total_ppt]}
+			end
+			# @observationsHash is populated with all raw data at this point.
+			# for each year, [0] will always be nil, and [60] will be nil during leap years
+			# Now need to:
+			# - collapse Dec 30 & 31 for leap years.
+			# - remove Feb 29 for non-leap years
+			require 'fileutils'
+			for station in @observationsHash.keys do
+				for year in @observationsHash[station].keys do
+					if Date.leap?(year) then # collapse Dec 30 & 31
+						@observationsHash[station][year][365][:tmin] = (@observationsHash[station][year][365][:tmin] + @observationsHash[station][year][366][:tmin]) / 2
+						@observationsHash[station][year][365][:tmax] = (@observationsHash[station][year][365][:tmax] + @observationsHash[station][year][366][:tmax]) / 2
+						@observationsHash[station][year][365][:precip] = (@observationsHash[station][year][365][:precip] + @observationsHash[station][year][366][:precip])
+						@observationsHash[station][year].delete_at(366)
+					else
+						@observationsHash[station][year].delete_at(60) # remove feb 29
 					end
-				end #creating csv
-			end #for year
-			# update coordinates file
-			File.open("/production/data/climate/stations/#{station}/coordinates.json","w"){ |f| f << coordinatesHash[station].to_json }
-		end #for station
+					@observationsHash[station][year].delete_at(0) # remove placeholder
+					# save file as CSV
+					dirname = "/production/data/climate/stations/#{station}/daily"
+					FileUtils.mkdir_p(dirname) # create all missing subdirectories
+					# Create a new CSV file populated with observations data
+					CSV.open("#{dirname}/#{year}.csv", 'w', col_sep: "\t") do |csv|
+						csv << ['tmin', 'tmax', 'precip'] # Add headers
+						@observationsHash[station][year].each do |row|
+							if row == nil then
+								errors.push({:station=>[station], :year=>[year]})
+							else
+								csv << [row[:tmin], row[:tmax], row[:precip]]
+							end
+						end
+					end #creating csv
+				end #for year
+				# update coordinates file
+				File.open("/production/data/climate/stations/#{station}/coordinates.json","w"){ |f| f << coordinatesHash[station].to_json }
+			end #for station
+		end
 		# - ensure that there are 365 days with no nils. 
 		if errors.size > 0 then
 			return errors
