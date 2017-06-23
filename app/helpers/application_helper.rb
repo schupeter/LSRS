16fd022d51e0ffb74ab2f7692e713eb3ed8dcc52
Peter Schut <schupeter@users.noreply.github.com>
2017-06-23 10:56:05 -0400

    Added version timestamp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+	# convert date (Time) into a human-readable text string
+	def lsrsVersionTimestamp()
+		`find "#{Rails.root.to_s}/app" -printf '%T+ %p\n' | sort -r | head -1`[0..9].delete('-')
+	end
+
 end
